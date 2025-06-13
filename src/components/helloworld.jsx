2af41ae611d87a5c08a83f5b256f6247d906cb57
Lucasmenezes08 <lms4@cesar.school>
2025-06-13 11:14:13 -0300

    :sparkles: feat: Adicionar primeiro componente, helloworld.

---
+++ src/components/helloworld.jsx
@@ -0,0 +1,8 @@
+export function Helloworld (){
+    return (
+        <section>
+            <h1>Bem vindo ao meu site!</h1>
+            <p>Aqui você encontra informacoes incriveis sobre react</p>
+        </section>
+    )
+}
